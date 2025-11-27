2fe2091f583b828b239b50fd67a448c40fb988cb
thething <atillzcomputers@gmail.com>
2025-11-27 04:00:13 -0500

    Add files via upload

---
+++ const.py
@@ -0,0 +1,8 @@
+"""Constants for the Surface WiFi Fix integration."""
+
+from __future__ import annotations
+
+DOMAIN = "surface_wifi_fix"
+DEFAULT_INTERFACE = "wlp3s0"
+SERVICE_DISABLE_POWER_SAVE = "disable_power_save"
+ATTR_INTERFACE = "interface"
